08421297f0a7ccf2608ce8a29be294455d3e64e1
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-12-01 10:13:53 +0700

    refactor: clean unused imports

--- lib/presentation/android/monitor/monitor_page.dart
+++ lib/presentation/android/monitor/monitor_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:simodang_v2/presentation/android/home/widgets/lists/pond_list_item_widget.dart';
 import 'package:simodang_v2/presentation/android/monitor/monitor_controller.dart';
 import 'package:simodang_v2/presentation/android/monitor/widgets/lists/pond_grid_widget.dart';
 
